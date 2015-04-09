3098860a4eda14593043882ca495f0e125e81e0d
Todd Robinson <toddrobinson1129@gmail.com>
2015-04-09 13:57:00 -0400

    Initial

---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('app')
+
+@section('title', 'Welcome')
+
+@section('content')
+</div>
+<div class="row homeHero">
+	<div class="col-md-6 col-md-offset-3 text-center">
+		<div class="welcomeText">
+			<h1>Welcome to IndoorSpinning</h1>
+			<span>Train inside on your bicycle</span>
+		</div>
+
+	</div>
+</div>
+<div class="container">
+
+@stop
