28cbb4125477772acea675fe30949af6b5a06616
Todd Robinson <toddrobinson1129@gmail.com>
2015-04-14 20:53:08 -0400

    Added function to fill out the interval data form using the individual interval form elements. Changed default value for length input. Added read only attr to total length and number of intervals. Removed the disabled attr, as that prevents submission.
    Signed-off-by: Todd Robinson <toddrobinson1129@gmail.com>

--- public/scripts/trackCreationForm.js
+++ public/scripts/trackCreationForm.js
@@ -12,6 +12,7 @@ $("form").on("click", "#removeInterval", function(){
 
 $("form").on("change", function(){
   timeAndData();
+  fillIntervalData();
 });
 
 $("div#intervalContainer").find(".intervalItem").first().slideDown();
@@ -24,7 +25,7 @@ function addInterval() {
   $("input#numberOfIntervals").val(numIntervals + 1);
   var nextInterval = numIntervals + 1;
   var title = "<p>Interval " + nextInterval  + "</p>";
-  var length = 'Length(minutes):<input required type="number" min="1" class="intervalLength"></input>';
+  var length = 'Length(minutes):<input required type="number" value="1" min="1" class="intervalLength"></input>';
   var intensity = 'Intensity(1-10)<input required type="number" min="1" max="10" class="intervalIntensity"></input>';
   var button = '<input class="btn btn-danger" id="removeInterval" type="button" value="Remove" readonly>'
   var intervalItem = $('<div class="intervalItem form-group" id='+ nextInterval + '>' + title  + length + intensity + button + '<hr></div>');
@@ -53,7 +54,7 @@ function reorderIntervals() {
 
   //Skipping the first one on purpose
   for(var i = 1 ; i< intervalSections.length ; i++) {
-    console.log(intervalSections[i]);
+    //console.log(intervalSections[i]);
     $(intervalSections[i]).attr("id", i+1);
     $(intervalSections[i]).find("p").text("Interval " + parseInt(i+1));
   }
@@ -73,4 +74,23 @@ function timeAndData() {
   else {
   $("form").find("input#length").val(totalTime);
   }
+}
+
+function fillIntervalData() {
+  var intervalSections = $("div#intervalContainer").find(".intervalItem");
+  var data = new Object();
+  var intervals = [];
+  for (var i = 0 ; i < intervalSections.length; i++) {
+    var id = i;
+    var intervalLength = $(intervalSections[i]).find(".intervalLength").val();
+    var intervalIntensity = $(intervalSections[i]).find(".intervalIntensity").val();
+    var tempInterval = {};
+    tempInterval.id = id;
+    tempInterval.intervalLength = intervalLength;
+    tempInterval.intervalIntensity = intervalIntensity;
+    intervals.push(tempInterval);
+  }
+  data.intervals = intervals;
+  $("form").find("#intervalData").val(JSON.stringify(data));
+  console.log(data);
 }
--- resources/views/tracks/create.blade.php
+++ resources/views/tracks/create.blade.php
@@ -12,7 +12,7 @@
   </div>
   <div class="form-group">
     {!! Form::label('numberOfIntervals' ,'Number of Intervals:')!!}
-    <input class="form-control" name="numberOfIntervals" id="numberOfIntervals" type="number" value="1" min="1" max="100" required disabled>
+    <input class="form-control" name="numberOfIntervals" id="numberOfIntervals" readonly type="number" value="1" min="1" max="100" required >
   </div>
   <div class="form-group">
     <input class="btn btn-default"  id="addInterval" type="button" value="Add interval" readonly>
@@ -23,14 +23,17 @@
   <div id="intervalContainer">
     <div class="intervalItem form-group" id="1">
       <p>Interval 1</p>
-      Length(minutes):<input required type="number" min="1" class="intervalLength">
+      Length(minutes):<input required type="number" value="1" min="1" class="intervalLength">
       Intensity(1-10)<input required min="1" max="10" type="number" class="intervalIntensity">
       <hr>
     </div>
   </div>
+  <div class="form-group">
+    <input class="btn btn-default"  id="addInterval" type="button" value="Add interval" readonly>
+  </div>
   <div class="form-group lengthGroup">
     {!! Form::label('length' ,'Length(min):')!!}
-    {!! Form::text('length', null, ['class' => 'form-control', 'disabled']) !!}
+    {!! Form::text('length', null, ['class' => 'form-control', 'readonly']) !!}
   </div>
   <div class="form-group">
     {!! Form::submit('Create Track', ['class' => 'btn btn-primary form-control']) !!}
