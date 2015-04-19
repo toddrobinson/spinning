c8e5eec97e07e642ba817d1203094051b1a9c30c
Todd Robinson <toddrobinson1129@gmail.com>
2015-04-19 18:35:20 -0400

    Changed brand color.
    Signed-off-by: Todd Robinson <toddrobinson1129@gmail.com>

--- resources/views/tracks/trackList.blade.php
+++ resources/views/tracks/trackList.blade.php
@@ -8,8 +8,8 @@
       @foreach ($tracks as $track)
       <article>
           <p><a href="/tracks/{{$track->id}}"><h2><small>Title: </small>{{$track->title}}</h2></a></p>
-          <p>{{$track->numberOfIntervals}} Intervals</p>
-          <p>{{$track->length}} minutes long</p>
+          <p><strong>{{$track->numberOfIntervals}}</strong> Intervals</p>
+          <p><strong>{{$track->length}}</strong> minutes long</p>
           <p>Created by <a href="/tracks/user/{{$track->user_id}}">{{$track->user->name}}</a> </p>
       </article>
       @endforeach
