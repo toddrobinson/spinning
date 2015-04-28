b5e3de30ff4a11e8a8618332932611477195395b
Todd Robinson <toddrobinson1129@gmail.com>
2015-04-28 15:16:46 -0400

    Handle user having no tracks
    Signed-off-by: Todd Robinson <toddrobinson1129@gmail.com>

--- resources/views/tracks/userTracks.blade.php
+++ resources/views/tracks/userTracks.blade.php
@@ -3,7 +3,11 @@
 @section('title', 'Tracks')
 @section('content')
 <div class="page-header">
+  @if(count($tracks)!= 0 )
   <h1>{{$tracks[0]->user->name}}'s Tracks</h1>
+  @else
+  <h1>Uh oh!</h1>
+  @endif
 </div>
 @include('tracks/trackList')
 
